0b1aedfef976d0bf60f0c264ccc82376b6d6ea1e
Navya Kumar <navyakumar27009@gmail.com>
2026-02-11 12:41:54 +0530

    feat: first assignment

---
+++ Assignment_1/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("#form")
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+    console.log(event_title.value)
+    console.log(event_date.value)
+    console.log(category.value)
+    console.log(description.value)
+    const eventCards=document.querySelector('.cards')
+        event.preventDefault()
+        const title=event_title.value
+        const date=event_date.value
+        const cat=category.value
+        const desc=description.value
+
+        const card=document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML=`
+        <h2>${title}</h2>
+        <p>📆${date}</p>
+        <button>${cat}</button>
+        <p>${desc}</p>
+        <div class="deleteCard">x</div>
+        `
+        card.querySelector('.deleteCard').addEventListener('click',function(){
+            card.remove()
+        })
+        
+        eventCards.appendChild(card)
+})
+
